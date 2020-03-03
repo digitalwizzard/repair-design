9c2a8557c9fcc96aba41d1eb07ea43ca55d75d22
digitalwizzard <blood.trasher@gmail.com>
2020-03-03 10:49:02 +0300

    add MinCSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+const cleanCSS = require('gulp-clean-css');
+const rename = require("gulp-rename");
 
 gulp.task('browser-sync', function() {
   browserSync.init({
@@ -10,7 +12,14 @@ gulp.task('browser-sync', function() {
   gulp.watch("./*.html").on('change', browserSync.reload);
 });
 
-gulp.task('hello', function(done) {
-  console.log('Привет мир!');
-  done();
-});
+gulp.task('mincss', function() {
+
+  return gulp.src("src/css/*.css")
+  
+  .pipe(rename({suffix: ".min"}))
+  
+  .pipe(cleanCSS())
+  
+  .pipe(gulp.dest("app/css"));
+  
+  })
