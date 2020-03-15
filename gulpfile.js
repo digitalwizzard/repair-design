e79208d1b0a903ad258ab54acdd88d12af73ea8b
digitalwizzard <blood.trasher@gmail.com>
2020-03-15 09:47:08 +0300

    feat: add jquery to modal

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
+'use strict';
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const cleanCSS = require('gulp-clean-css');
 const rename = require("gulp-rename");
+const sass = require('gulp-sass');
+const watchSass = require('gulp-watch-sass')
+const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('browser-sync', function() {
   browserSync.init({
@@ -20,6 +24,26 @@ gulp.task('mincss', function() {
   
   .pipe(cleanCSS())
   
-  .pipe(gulp.dest("app/css"));
-  
-  })
+  .pipe(gulp.dest("app/css"));  
+  });
+
+
+  gulp.task('sass', function (done) {
+    gulp.src('./sass/**/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('./css'));
+      done();
+  });
+   
+  gulp.task('sass:watch', function () {
+    gulp.watch('./sass/**/*.scss', ['sass']);
+  });
+
+
+exports.default = () => (
+    gulp.src('src/app.css')
+        .pipe(autoprefixer({
+            cascade: false
+        }))
+        .pipe(gulp.dest('dist'))
+);
