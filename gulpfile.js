58cabb7d061108eeb81b91309c95c7481bb29830
digitalwizzard <blood.trasher@gmail.com>
2020-03-20 21:29:55 +0300

    add valid forms

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,7 @@
-'use strict';
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const cleanCSS = require('gulp-clean-css');
 const rename = require("gulp-rename");
-const sass = require('gulp-sass');
-const watchSass = require('gulp-watch-sass')
-const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('browser-sync', function() {
   browserSync.init({
@@ -24,26 +20,6 @@ gulp.task('mincss', function() {
   
   .pipe(cleanCSS())
   
-  .pipe(gulp.dest("app/css"));  
-  });
-
-
-  gulp.task('sass', function (done) {
-    gulp.src('./sass/**/*.scss')
-      .pipe(sass().on('error', sass.logError))
-      .pipe(gulp.dest('./css'));
-      done();
-  });
-   
-  gulp.task('sass:watch', function () {
-    gulp.watch('./sass/**/*.scss', ['sass']);
-  });
-
-
-exports.default = () => (
-    gulp.src('src/app.css')
-        .pipe(autoprefixer({
-            cascade: false
-        }))
-        .pipe(gulp.dest('dist'))
-);
+  .pipe(gulp.dest("app/css"));
+  
+  })
